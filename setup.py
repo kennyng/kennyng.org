119cb3aadb8e65c497e11b715c8690bdd79af585
Kenny Ng <kennyng.zq@gmail.com>
2015-05-25 01:23:57 -0500

    Added repo and package setup/config files.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='www.kennyng.org',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Personal website of Kenny Ng',
+      author='Kenny Ng',
+      author_email='kennyng.zq@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      install_requires=['Flask>=0.10.1'],
      )
