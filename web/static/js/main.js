fe73a048abe9ecfd3cafacd4a261dba9483b7201
Kenny Ng <kennyng.zq@gmail.com>
2015-06-06 04:34:08 -0500

    Added responsive mobile menu.

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -4,3 +4,14 @@ var _ = {
     return document.getElementById(id);
   }
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+  var menu = _.$('nav-menu-bar')
+  var nav = _.$('site-nav');
+  
+  menu.addEventListener('click', function(e) {
+    nav.classList.toggle('visible');
+    menu.classList.toggle('color-menu-open');
+  }, false);
+
+}, false);
