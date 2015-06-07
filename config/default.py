4ea65b0c6c634d52a90f7753c22c97f5af352795
Kenny Ng <kennyng.zq@gmail.com>
2015-06-07 02:58:21 -0500

    Changed development database config.

--- config/default.py
+++ config/default.py
@@ -14,14 +14,12 @@ SALT = os.environ.get('FLASK_SALT', 'default-pw-salt')
 PASSWORD = os.environ.get('FLASK_PASSWORD', ('pbkdf2:sha1:1000$6b0JlqVL$2779dc6'
                                              '5e4ac3dcc016b6ffdd97cb69a431ca6c7'))
 
-
-PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-APP_DIR = os.path.join(PROJECT_DIR, 'web')
-DATABASE = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', APP_DIR),
-                                       'sqlite3.db')
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(DATABASE)
-
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'web')
 IP = os.environ.get('OPENSHIFT_PYTHON_IP', '127.0.0.1')
 PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT', 8080))
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATABASE = os.path.join(os.environ.get('OPENSHIFT_DATA_DIR', PROJECT_DIR),
+                                       '{}.db'.format(APP_NAME))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(DATABASE)
